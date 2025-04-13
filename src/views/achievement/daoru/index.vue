723e2c79d39ad7826f41bde3afe1c8feb5e1a89a
tangcong <1432339230@qq.com>
2025-04-13 12:03:28 +0800

    feat: (导入事例新增)

--- src/views/achievement/daoru/index.vue
+++ src/views/achievement/daoru/index.vue
@@ -7,7 +7,57 @@
 <!--      <span class="span2">代表あいさつ</span>-->
     </div>
     <div class="content">
-
+      <div class="item_content">
+        <div class="item">
+          <div class="span1">インバウンド対応システム導入で見えた未来</div>
+          <div class="span1">― 人気ラーメンチェーン店が選んだ次世代ソリューション ―</div>
+          <div class="cont">
+            インタビューに応じていただいたのは、東京都内を拠点に全国13店舗を展開する人気ラーメンチェーン店の代表A様。少数精鋭で運営される店舗が抱える課題と、それを解決したシステム導入の背景について伺いました。
+          </div>
+          <br/>
+        </div>
+        <div class="item">
+          <div class="span1">現場の課題とシステム導入のきっかけ</div>
+          <div class="cont">Q. システム導入を考え始めたきっかけを教えてください。</div>
+          <div class="cont">A様：</div>
+          <div class="cont">
+            <span>「うちはカウンター席のみの店舗で、ホールスタッフがいません。そのため、キッチンスタッフが調理や接客に加え、行列の状況確認やお客様の案内まで行う必要がありました。この手間が業務効率に大きく影響していて、なんとか改善したいと思っていました。」</span><br/><br/>
+            <span>「また、インバウンドのお客様が増える中で、多言語対応や注文時のトラブルが増えていて、スタッフの負担が限界に近づいていました。これが直接的なきっかけです。」</span>
+          </div>
+          <br/>
+        </div>
+        <div class="item">
+          <div class="span1">選定理由と決め手</div>
+          <div class="cont">Q. ブリッジランドのシステムを選んだ理由は何だったのでしょうか？</div>
+          <div class="cont">A様：</div>
+          <div class="cont">
+            <span>「まず、スマホ1台で順番待ち、注文、決済まで一括対応できる点に惹かれました。お客様が使っているスマホの言語に自動対応してくれるので、言語の壁を感じることもありません。」</span><br/><br/>
+            <span>「さらに、順番待ちやオーダー、決済をそれぞれ別の業者に頼むことなく、一つのシステムで完結できるのは大きな魅力でした。実際に導入した結果、スタッフの負担も大幅に減り、1人分の人件費も削減できたのは助かっています。」</span>
+          </div>
+          <br/>
+        </div>
+        <div class="item">
+          <div class="span1">導入後の変化と効果</div>
+          <div class="cont">Q. 実際に導入して、どのような変化がありましたか？</div>
+          <div class="cont">A様：</div>
+          <div class="cont">
+            <span>「一番の変化は、スタッフが余裕を持てるようになったことですね。特にキッチン業務に専念できるようになり、お客様への提供スピードが向上しました。また、お客様が自分のペースでオーダーや決済を行えるようになり、トラブルも減少。顧客満足度が上がったと感じています。」</span><br/><br/>
+            <span>「店舗としても、業務効率が上がると同時にコスト削減が実現しました。今後は系列店舗にもシステムを導入して、インバウンド顧客をさらに取り込みたいと思っています。」</span>
+          </div>
+          <br/>
+        </div>
+        <div class="item">
+          <div class="span1">今後の展望</div>
+          <div class="cont">Q. 今後どのような目標を掲げていますか？</div>
+          <div class="cont">A様：</div>
+          <div class="cont">
+            <span>「当店は、日本全国に13店舗展開していますが、それぞれが異なる味や雰囲気を持つユニークな店舗です。まずは、インバウンドのお客様にもこうした魅力をもっと知っていただき、ぜひ他の店舗にも足を運んでいただけるようにしたいと考えています。」</span><br/><br/>
+            <span>「また、台湾やアジア圏、アメリカなど海外進出を果たした店舗もありますので、ブリッジランドさんのインバウンド集客サービスを活用して、国内外の認知度をさらに向上させたいですね。訪日のインバウンドのお客様に当店を知っていただき、自国に戻った際には周囲にその体験を伝えたり、現地の店舗にも足を運んでいただけるようになればと期待しています。日本国内での認知度向上が、海外進出店舗の活性化やビジネスの拡大にも繋がると信じています。」</span><br/><br/>
+            <span>「こうした取り組みを通じて、国内外で当店のファンを増やし、さらに多くの方に私たちのラーメンを楽しんでいただきたいですね。これからも新たな市場に挑戦しながら、店舗の魅力をグローバルに発信していきたいですね。」</span>
+          </div>
+          <br/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,16 +82,44 @@
     }
   }
   .content{
-    text-align: left;
-    p{
-      line-height: 1.875rem;
-      font-size: 0.9375rem;
+    .item_content{
+      text-align: left;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      .item{
+        width: 80%;
+        .span1{
+          font-size: 1.125rem;
+          font-weight: 700;
+          font-family: Roboto Condensed, sans-serif;
+          margin-top: 0.8rem;
+          margin-bottom: 0.5rem;
+        }
+        .cont{
+          line-height: 1.875rem;
+          font-size: 14px;
+          margin: 0;
+        }
+      }
     }
   }
 }
 @media screen and (max-width:1500px) {
   .mu_box{
     padding: 0 6rem;
+  }
+}
+@media screen and (max-width:2100px) {
+  .mu_box{
+    .content{
+      .item_content{
+        .item{
+          width: 100%;
+        }
+      }
+    }
   }
 }
 @media screen and (max-width:991px) {
@@ -67,8 +145,10 @@
       }
     }
     .content{
-      p{
-        font-size: 0.875rem;
+      .item_content{
+        .item{
+          width: 100%;
+        }
       }
     }
   }
--- src/views/qa/index.vue
+++ src/views/qa/index.vue
@@ -22,34 +22,41 @@ import Bottom from "@/components/Bottom.vue";
               ・特徴：メッセージ、決済、ECなど多機能を備えたスーパーアプリ。日常生活に欠かせない存在です。<br/>
               ・ユーザー層：幅広い年齢層、特に20代以上の社会人に人気。
             </span>
+            <br/>
             <span class="span2">
               Weibo（微博）<br/>
               ・特徴：リアルタイム性・拡散力が高く、企業や著名人も多く利用するSNS。<br/>
               ・ユーザー層：20代～30代中心。トレンドやニュースに敏感。
             </span>
+            <br/>
             <span class="span2">
               RED（小紅書）<br/>
               ・特徴：口コミ投稿型SNS。特に美容・ファッション・旅行情報の共有が活発です。<br/>
               ・ユーザー層：20代～30代の女性が中心で、実用的な情報を重視。
             </span>
+            <br/>
             <span class="span2">
               TikTok（抖音）<br/>
               ・特徴：短尺動画プラットフォームで、エンタメ・音楽・ダンス動画が中心。<br/>
               ・ユーザー層：10代～20代の若年層がメインで、クリエイティブなコンテンツを好みます。
             </span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国ではFacebookやInstagramは使えますか？</span>
             <span class="span2">A：中国国内では、FacebookやInstagramは利用できません。代わりにWeChatやWeiboなどが主に使われています。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国人が飲食店を探すとき、どのアプリを使いますか？</span>
             <span class="span2">A：飲食店探しには「大衆点評」が一般的です。月間アクティブユーザー数（MAU）は約2億人で、口コミや評価を見て飲食店を選ぶのが主流です。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国人は海外旅行の際、どうやってホテルを予約していますか？</span>
             <span class="span2">A：「Ctrip（携程旅行網）」を利用する方が大多数です。Ctripは中国最大級の旅行予約サイトで、会員数は4億人以上。多くの中国人が国内外のホテル予約に活用しています。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国ではどのチャネルでマーケティングを行うべきですか？</span>
             <span class="span2">A：効果的なマーケティングは複数のチャネルを組み合わせることが重要です。<br/>
@@ -60,6 +67,7 @@ import Bottom from "@/components/Bottom.vue";
 　                               ・旅行：Ctrip、Qunar<br/>
 　                               ・ショッピング：大衆点評、Tmall</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：マーケティングサポートはお願いできますか？</span>
             <span class="span2">A：はい、可能です！<br/>
@@ -77,30 +85,35 @@ import Bottom from "@/components/Bottom.vue";
           <div class="item_a">
             <span class="span2a">Q：導入のメリットは何ですか？</span>
             <span class="span2">A：当社のインバウンド対応システムを導入することで、以下のメリットがあります。</span>
-            <span class="span2">
-              ・業務効率化：順番待ち管理、モバイルオーダー、セルフ決済をワンストップで提供し、オペレーション負担を軽減します。<br/>
+            <div class="span2">
+              <div>・業務効率化：順番待ち管理、モバイルオーダー、セルフ決済をワンストップで提供し、オペレーション負担を軽減します。</div>
               ・売上アップ：訪日外国人観光客向けの多言語対応により、新規顧客の獲得とリピーターの増加が期待できます。日本人のお客様にもスムーズにご利用いただけるため、幅広い客層へのサービス提供が可能です。<br/>
               ・コスト削減：人手不足対策として、業務の自動化・省力化を実現します。<br/>
               ・集客力強化：中国SNS運用代行とのセットプランで、効率的に訪日客へのPRが可能です。
-            </span>
+            </div>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：導入にあたって、準備する機材はありますか？</span>
             <span class="span2">A：特別なご用意は不要です。必要な機材はすべて当社でご用意いたします。</span><br/>
             <span class="span2">※導入時に30,000円のデポジットをお預かりいたしますが、店舗の累計売上が200万円に達した時点で全額返金いたします。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：申し込みからどのくらいで利用を開始できますか？</span>
             <span class="span2">A：最短2週間ほどでご利用いただけます。導入スケジュールは柔軟に対応いたしますので、お気軽にご相談ください。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：基本プランの内容を教えてください。</span>
             <span class="span2">A：順番待ち管理、モバイルオーダー、セルフ決済、予約管理など、飲食店の運営に役立つ機能をワンストップで提供します。ご要望に応じたカスタマイズも可能です。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：来店前のPRや集客支援もお願いできますか？</span>
             <span class="span2">A：はい、中国SNS運用代行とシステム利用がセットになったお得なプランもご用意しています。お気軽にお問い合わせください。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：契約期間はどのくらいですか？</span>
             <span class="span2">A：基本は1年契約となりますが、まずは気軽にお試しいただける3ヶ月間のトライアルプランもございます。</span>
@@ -125,10 +138,12 @@ import Bottom from "@/components/Bottom.vue";
               ・多言語対応の強化：多言語対応で訪日外国人観光客の取り込みがスムーズに行えます。
             </span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：運用可能な施設を教えてください。</span>
             <span class="span2">A：ホテルから小規模施設まで、幅広い施設に運用代行サービスを提供いたします。中国最大のOTAサイト「Ctrip」をはじめ、SNSやグローバルOTAサイトでの販売まで対応可能です。また、民泊運営代行サービスも展開しております。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：運用可能なプラットフォームを教えてください。</span>
             <span class="span2">
@@ -136,6 +151,7 @@ import Bottom from "@/components/Bottom.vue";
                 SNS：「WeChat」「TikTok（抖音）」「RED（小紅書）」
             </span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：運用内容を教えてください。</span>
             <span class="span2">A：以下の業務を包括的にサポートいたします。</span>
@@ -148,14 +164,17 @@ import Bottom from "@/components/Bottom.vue";
               ・施設担当者との打ち合わせ・レポート提出
             </span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：PMS（ホテルシステム）の対応はできますか？</span>
             <span class="span2">A：はい、ご提案からシステム構築まで幅広く対応可能です。お気軽にご相談ください。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：ブログや中国観光客向けのSNS運用代行はできますか？</span>
             <span class="span2">A：はい、SNS運用代行プランもご用意しております。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：OTA担当者とのやりとりもサポートしてもらえますか？</span>
             <span class="span2">A：はい、OTA担当者とのやりとりもサポートいたします。また、担当者が不在の場合でも、弊社がOTA部門として業務を一括してお任せいただけます。</span>
@@ -173,14 +192,17 @@ import Bottom from "@/components/Bottom.vue";
             <span class="span2a">Q：専門領域は何ですか？</span>
             <span class="span2">A：旅行・観光業務全般を専門としており、特にインバウンド集客に関する知見を活かしたコンサルティングを提供しています。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国OTAを活用する際、どのような初期準備が必要ですか？</span>
             <span class="span2">A：初期準備では、ターゲットとなる顧客層の明確化が最も重要です。その上で、OTAプラットフォームに適した商品・サービスの最適化、競合分析を行い、OTAのアルゴリズムに対応した店舗情報の登録や、レビュー促進の仕組み作りも欠かせません。当社では、これらのプロセス全てを包括的にサポートいたします。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：具体的にどのような形で進められますか？</span>
             <span class="span2">A：初回ヒアリングを通じてお客様の目標や現状を深く理解し、その情報を基に、最適な戦略を個別に策定いたします。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：中国OTAの販売戦略を依頼すると、具体的にどのような成果が期待できますか？</span>
             <span class="span2">A：効果的なプロモーション戦略によってインバウンド集客力を向上させ、また、予約率を高めるためにプランの最適化を行います。これにより、売上や集客目標の達成を実現できるよう、実践的なサポートを行います。</span>
@@ -198,10 +220,12 @@ import Bottom from "@/components/Bottom.vue";
             <span class="span2a">Q：HHトラベルはどのようなプレミアム旅行商品を提供していますか？</span>
             <span class="span2">A：HHトラベルでは、中国の富裕層をターゲットにしたプレミアム旅行商品を企画・販売しています。例えば、日本の伝統文化を体験できる特別なツアーや、ミシュラン星付きレストランでの食事など、他では味わえないユニークで洗練された旅行体験を提供しています。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：このサービスを利用すると、私たちのビジネスにどのような利点がありますか？</span>
             <span class="span2">A：我々との提携により、高単価な中国富裕層のお客様に直接アプローチでき、収益の成長やブランド価値の向上が期待できます。ターゲット層への効果的なアプローチが可能となり、ビジネスの新たな発展を促進します。</span>
           </div>
+          <br/>
           <div class="item_a">
             <span class="span2a">Q：パートナーシップを開始するには、どのようなプロセスが必要ですか？</span>
             <span class="span2">A：まずは、お客様の旅行事業や商品内容を理解するために初期相談を行います。その後、お客様のニーズに応じた商品企画を立案し、HHトラベルのネットワークを活用して中国富裕層への販売を開始します。具体的なサポート内容については、お問い合わせいただければ、詳細を丁寧にご説明いたします。</span>
